52dfbac4838eb1d6e1c23dcd08308aa673c588cd
szymmik <72349273+szymmik@users.noreply.github.com>
2023-04-26 10:59:18 +0200

    SRC i pom
    lab

---
+++ src/main/java/pl/zajecia/jenkinsheroku/JenkinsherokuApplication.java
@@ -0,0 +1,20 @@
+package pl.zajecia.jenkinsheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JenkinsherokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsherokuApplication.class, args);
+	}
+
+	@GetMapping
+	public String get(){
+		return "Hello Jenkins";
+	}
+}
---
+++ src/test/java/pl/zajecia/jenkinsheroku/JenkinsherokuApplicationTests.java
@@ -0,0 +1,30 @@
+package pl.zajecia.jenkinsheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.StatusAssertions;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class JenkinsherokuApplicationTests {
+@Autowired
+private MockMvc mockMvc;
+	@Test
+	void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello Jenkins"));
+	}
+
+
+
+}
